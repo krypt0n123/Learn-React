914d07008b10cf372acd3a00f5ca2edd773809d8
krypt0n123 <352600525@qq.com>
2025-04-13 23:31:55 +0800

    init

---
+++ Chat-app/app/chat/page.tsx
@@ -0,0 +1,7 @@
+export default function Chat() {
+  return (
+    <main className="bg-blue-400 p-10">
+      <h1>Cha</h1>
+    </main>
+  )
+}
---
+++ Chat-app/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="bg-yellow-400 p-10">
+      <h1>Hello react</h1>
+    </main>
+  )
+}
