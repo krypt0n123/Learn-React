5edc72002ebcdff7750147d26f35d178605f031c
krypt0n123 <352600525@qq.com>
2025-04-26 18:50:39 +0800

    4.26-2

--- Chat-app/components/home/Main/Menu.tsx
+++ Chat-app/components/home/Main/Menu.tsx
@@ -6,24 +6,23 @@ import { ActionType } from "@/reducers/AppReducer"
 import { LuPanelLeft } from "react-icons/lu"
 
 export default function Menu() {
-    const {
-        state: { displayNavigation },
-        dispatch
-    } = useAppContext()
-    return (
-      <Button
-        icon={LuPanelLeft}
-        className={`${
-          displayNavigation ? "hidden" : ""
+  const {
+    state: { displayNavigation },
+    dispatch
+  } = useAppContext()
+  return (
+    <Button
+      icon={LuPanelLeft}
+      className={`${displayNavigation ? "hidden" : ""
         } fixed left-2 top-2`}
-        variant='outline'
-        onClick={() => {
-          dispatch({
-            type: ActionType.UPDATE,
-            field: "displayNavigation",
-            value: true
-          })
-        }}
-      />
-    )
+      variant='outline'
+      onClick={() => {
+        dispatch({
+          type: ActionType.UPDATE,
+          field: "displayNavigation",
+          value: true
+        })
+      }}
+    />
+  )
 }
